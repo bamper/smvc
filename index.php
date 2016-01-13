6fb038eb5275dfec8c848a6a9f116b9a70aacc16
Evchev Ruslan <aion.planet.com@gmail.com>
2016-01-13 10:09:01 +0200

    add initialize index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include 'vendor/autoload.php';
+ini_set('display_errors', 1);
+
+use Symfony\Component\Debug;
+use SMVC\Router\Router;
+use SMVC\Core\Bootstrap;
+
+Debug\Debug::enable(1);
+
+$bootstrap = new Bootstrap();
+$bootstrap->run();
+
+Router::get('/main/edit', 'MainController@edit');
+Router::post('/', 'MainController@index');
+
+Router::getInstance();
