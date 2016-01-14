770b6d0975a8be5cce09d05c8dd9386d47021143
Evchev Ruslan <aion.planet.com@gmail.com>
2016-01-14 18:07:15 +0200

    ORM QueryBuilder

--- vendor/SMVC/Core/Kernel/Database.php
+++ vendor/SMVC/Core/Kernel/Database.php
@@ -6,16 +6,9 @@ use SMVC\Core\Registry;
 
 class Database
 {
-    public static $db;
 
     public static function getConnection()
     {
-        self::connect();
-        return self::$db;
-    }
-
-    private static function connect()
-    {
-        self::$db = Registry\Registry::get('db');
+        return Registry\Registry::get('db');
     }
 }
